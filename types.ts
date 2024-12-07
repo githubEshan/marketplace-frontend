340291e33a94400f2d00047a0b7b339f3ba7ada9
githubEshan <66063996+githubEshan@users.noreply.github.com>
2024-12-07 14:57:26 -0500

    set up get categories

--- types.ts
+++ types.ts
@@ -9,3 +9,14 @@ export interface Category{
     name: String;
     billboard: Billboard;
 };
+
+//export interface Product{
+  //  id: string;
+    //name: string;
+    //description: string
+    //images: Images[]
+//}
+
+export interface Images{
+
+}
